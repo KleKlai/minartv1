f439b20c9dd82c4a6a8e011562d36156d3f832dc
kleklai <mmagallen@klaiklai.tech>
2020-09-02 20:28:13 +0800

    enhance

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Mindanao Art') }} - @yield('title')</title>
+    <title>{{ config('app.name', 'Mindanao Art') }} @yield('title')</title>
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('/images/nav.ico') }}">
